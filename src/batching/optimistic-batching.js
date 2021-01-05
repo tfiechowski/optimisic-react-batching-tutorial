3e3a57235d8ca22612fcf24ad970bc373386df9f
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2021-01-05 22:43:21 +0100

    Add optimistic batching

---
+++ src/batching/optimistic-batching.js
@@ -0,0 +1,132 @@
+/* eslint-disable react/prop-types */
+import update from "immutability-helper";
+import { isEqual, omit } from "lodash";
+import { useCallback, useState } from "react";
+import { useDebouncedCallback } from "use-debounce/lib";
+
+const DEBOUNCED_BATCH_TIMEOUT = 500;
+
+function compareItems(original, updated) {
+  const fieldsToOmit = ["pending"];
+  return isEqual(omit(original, fieldsToOmit), omit(updated, fieldsToOmit));
+}
+
+function getPhotosToUpdate(photos, batchUpdates) {
+  function getPhoto(id) {
+    return photos.find((photo) => photo.id === id);
+  }
+
+  return Object.values(batchUpdates)
+    .map((batchUpdate) => {
+      const original = getPhoto(batchUpdate.id);
+
+      if (compareItems(original, batchUpdate)) {
+        return null;
+      }
+
+      return batchUpdate;
+    })
+    .filter((item) => item !== null);
+}
+
+export function usePhotos({ photos: initialPhotos = [], onUpdate }) {
+  const [photos, setPhotos] = useState(initialPhotos);
+  const [batchUpdates, setBatchUpdates] = useState({});
+
+  const updatePhotosDebounced = useDebouncedCallback(
+    async () => {
+      if (Object.keys(batchUpdates).length === 0) {
+        return;
+      }
+
+      setBatchUpdates((_batchUpdates) =>
+        update(_batchUpdates, {
+          $unset: Object.keys(batchUpdates),
+        })
+      );
+
+      setPhotos((_photos) => {
+        const resultPhotos = _photos.map((photo) => {
+          const key = photo.id;
+          const batchUpdateItem = batchUpdates[key];
+
+          if (batchUpdateItem) {
+            // Pending will be used to block the item from clicking on it again
+            return Object.assign({}, batchUpdateItem, { pending: true });
+          }
+          return photo;
+        });
+
+        return resultPhotos;
+      });
+
+      // Calling the API
+      try {
+        const photosToUpdate = getPhotosToUpdate(photos, batchUpdates);
+        
+        await onUpdate(photosToUpdate);
+        
+        // Reset pending
+        setPhotos((_photos) =>
+          _photos.map((photo) => {
+            const item = batchUpdates[photo.id] || photo;
+
+            return Object.assign({}, item, { pending: false });
+          })
+        );
+      } catch (exception) {
+        setPhotos((_photos) => {
+          return _photos.map((item) => {
+            const originalItem =
+            photos.find((photo) => photo.id === item.id) || item;
+
+            return Object.assign({}, originalItem, { pending: false });
+          });
+        });
+      }
+    },
+    DEBOUNCED_BATCH_TIMEOUT,
+    { maxWait: 2500 }
+  );
+
+  const handleMultipleChange = useCallback(
+    (items) => {
+      const { toReset, toUpdate } = items.reduce(
+        (acc, item) => {
+          const key = item.id;
+          const originalPhoto = photos.find((photo) => photo.id === item.id);
+
+          if (compareItems(originalPhoto, item)) {
+            return update(acc, { toReset: { $push: [key] } });
+          }
+
+          const updatedItem = Object.assign({}, originalPhoto, item);
+
+          return update(acc, {
+            toUpdate: {
+              [key]: { $set: updatedItem },
+            },
+          });
+        },
+        { toReset: [], toUpdate: {} }
+      );
+
+
+      setBatchUpdates(
+        update(batchUpdates, {
+          $unset: toReset,
+          $merge: toUpdate,
+        })
+      );
+
+      updatePhotosDebounced.callback();
+    },
+    [photos, batchUpdates, setBatchUpdates, updatePhotosDebounced]
+  );
+
+  return {
+    handleEdit: handleMultipleChange,
+    batchUpdates,
+    photos,
+  };
+}
---
+++ src/batching/optimistic-batching.test.js
@@ -0,0 +1,145 @@
+import { useCallback } from "react";
+import { usePhotos } from "./optimistic-batching";
+import { renderHook } from "@testing-library/react-hooks";
+import TestRenderer from "react-test-renderer";
+const { act } = TestRenderer;
+
+const DEFAULT_PHOTOS = [
+  { id: "1", title: "Photo #1", liked: false },
+  { id: "2", title: "Photo #2", liked: false },
+  { id: "3", title: "Photo #3", liked: false },
+  { id: "4", title: "Photo #4", liked: false },
+  { id: "5", title: "Photo #5", liked: false },
+];
+
+describe("Optimistic batching", () => {
+  beforeEach(() => {
+    jest.useFakeTimers("modern");
+  });
+
+  it("should do a batch update", async () => {
+    const onUpdate = () => true;
+    const { result, waitForNextUpdate } = renderHook(() =>
+      usePhotos({ photos: DEFAULT_PHOTOS, onUpdate })
+    );
+
+    expect(result.current.photos).toMatchSnapshot();
+
+    act(() => {
+      result.current.handleEdit([
+        { id: "1", liked: true },
+        { id: "3", liked: true },
+      ]);
+    });
+
+    await act(async () => {
+      jest.runAllTimers();
+      await waitForNextUpdate();
+    });
+
+    expect(result.current.photos).toMatchSnapshot();
+  });
+
+  it("should debounce batch update", async () => {
+    const onUpdate = () => true;
+    const { result, waitForNextUpdate } = renderHook(() =>
+      usePhotos({ photos: DEFAULT_PHOTOS, onUpdate })
+    );
+    function getLikedPhotosNumber() {
+      return result.current.photos.filter((photo) => photo.liked).length;
+    }
+
+    act(() => {
+      result.current.handleEdit([
+        { id: "1", liked: true },
+        { id: "2", liked: true },
+      ]);
+    });
+
+    await act(async () => {
+      jest.advanceTimersByTime(300);
+    });
+
+    expect(getLikedPhotosNumber()).toBe(0);
+
+    act(() => {
+      result.current.handleEdit([
+        { id: "3", liked: true },
+        { id: "4", liked: true },
+      ]);
+    });
+
+    await act(async () => {
+      jest.advanceTimersByTime(300);
+    });
+
+    expect(getLikedPhotosNumber()).toBe(0);
+
+    await act(async () => {
+      jest.runAllTimers();
+      await waitForNextUpdate();
+    });
+
+    expect(getLikedPhotosNumber()).toBe(4);
+  });
+
+  it("should mark items as pending while API call is being processed", async () => {
+    const onUpdate = () => new Promise((resolve) => setTimeout(resolve, 2000));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      usePhotos({ photos: DEFAULT_PHOTOS, onUpdate })
+    );
+    function getPendingPhotos() {
+      return result.current.photos.filter((photo) => photo.pending);
+    }
+
+    act(() => {
+      result.current.handleEdit([
+        { id: "1", liked: true },
+        { id: "2", liked: true },
+      ]);
+    });
+
+    act(() => {
+      jest.advanceTimersByTime(600);
+    });
+
+    expect(getPendingPhotos()).toMatchSnapshot();
+
+    await act(async () => {
+      jest.runAllTimers();
+      await waitForNextUpdate();
+    });
+
+    expect(result.current.photos).toMatchSnapshot();
+  });
+
+  it("should revert items after failed API call", async () => {
+    const onUpdate = () => new Promise((_, reject) => setTimeout(reject, 2000));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      usePhotos({ photos: DEFAULT_PHOTOS, onUpdate })
+    );
+    function getPendingPhotos() {
+      return result.current.photos.filter((photo) => photo.pending);
+    }
+
+    act(() => {
+      result.current.handleEdit([
+        { id: "1", liked: true },
+        { id: "2", liked: true },
+      ]);
+    });
+
+    act(() => {
+      jest.advanceTimersByTime(600);
+    });
+
+    expect(getPendingPhotos()).toMatchSnapshot();
+
+    await act(async () => {
+      jest.runAllTimers();
+      await waitForNextUpdate();
+    });
+
+    expect(result.current.photos).toMatchSnapshot();
+  });
+});
